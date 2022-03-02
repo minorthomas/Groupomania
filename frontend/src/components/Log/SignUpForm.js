178ba6bce286fb44afd9db8402b007a0f8af9da4
MINOR Thomas <minorthomas1@gmail.com>
2022-03-02 17:21:17 +0100

    Add appcontext

--- frontend/src/components/Log/SignUpForm.js
+++ frontend/src/components/Log/SignUpForm.js
@@ -28,7 +28,7 @@ const SignUpForm = () => {
     } else {
       await axios({
         method: "post",
-        url: `${process.env.REACT_APP_API_URL}user/register`,
+        url: `${process.env.REACT_APP_API_URL}api/user/register`,
         withCredentials: true,
         data: {
           firstname,
