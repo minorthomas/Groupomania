9a97d14981e6cbfb93dbfc80b4387eb26f9da46f
MINOR Thomas <minorthomas1@gmail.com>
2022-03-02 20:12:07 +0100

    add update bio

--- frontend/src/actions/user.actions.js
+++ frontend/src/actions/user.actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_USER = "GET_USER";
 export const UPLOAD_PICTURE = "UPLOAD_PICTURE";
+export const UPDATE_BIO = "UPDATE_BIO";
 
 export const getUser = (uid) => {
   return (dispatch) => {
@@ -30,5 +31,22 @@ export const uploadPicture = (data, id) => {
       .catch((error) => {
         console.log(error);
       });
+  };
+};
+
+export const updateBio = (userId, bio) => {
+  return (dispatch) => {
+    return axios({
+      method: "put",
+      url: `${process.env.REACT_APP_API_URL}api/user/` + userId,
+      data: { bio },
+    })
+      .then((res) => {
+        dispatch({
+          type: UPDATE_BIO,
+          payload: bio,
+        });
+      })
+      .catch((error) => console.log(error));
   };
 };
--- frontend/src/components/Profile/UpdateProfile.js
+++ frontend/src/components/Profile/UpdateProfile.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateBio } from "../../actions/user.actions";
 import LeftNav from "../LeftNav";
 import UploadImg from "./UploadImg";
 
 const UpdateProfile = () => {
+  const [bio, setBio] = useState("");
+  const [updateForm, setUpdateForm] = useState(false);
   const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleUpdate = () => {
+    dispatch(updateBio(userData.user.id, bio));
+    setUpdateForm(false);
+  };
 
   return (
     <div className="profil-container">
@@ -15,6 +24,29 @@ const UpdateProfile = () => {
           <h3>Photo de profil</h3>
           <img src="#" alt="Utilisateur" />
           <UploadImg />
+        </div>
+        <div className="right-part">
+          <div className="bio-update">
+            <h3>Bio</h3>
+            {updateForm === false && (
+              <>
+                <p onClick={() => setUpdateForm(!updateForm)}>Bio</p>
+                <button onClick={() => setUpdateForm(!updateForm)}>
+                  Modifier bio
+                </button>
+              </>
+            )}
+            {updateForm && (
+              <>
+                <textarea
+                  type="text"
+                  defaultValue="Bio"
+                  onChange={(event) => setBio(event.target.value)}
+                ></textarea>
+                <button onClick={handleUpdate}>Valider</button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
