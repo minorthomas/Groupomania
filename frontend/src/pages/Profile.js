02cdb696f2442208ecf991e5ca33e70a8f6e0ed5
MINOR Thomas <minorthomas1@gmail.com>
2022-03-02 17:24:22 +0100

    Test if the user is logged in

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Log from "../components/Log";
+import { UidContext } from "../components/AppContext";
 
 const Profile = () => {
+  const uid = useContext(UidContext);
+
   return (
     <div className="profil-page">
-      <div className="log-container">
-        <Log />
-        <img src="#" alt="Login" />
-      </div>
+      {uid ? (
+        <h1>Update</h1>
+      ) : (
+        <div className="log-container">
+          <Log />
+          <img src="#" alt="Login" />
+        </div>
+      )}
     </div>
   );
 };
